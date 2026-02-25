33386283a02814292524b25a343b29d0807783ff
Melany Alfaro <melanygabriela.alfaro@ucr.ac.cr>
2026-02-25 17:18:22 -0600

    feat: add the text to the reading section

--- src/pages/Student/quests/ReadingSection.jsx
+++ src/pages/Student/quests/ReadingSection.jsx
@@ -1,7 +1,8 @@
-export function ReadingSection() {
+export function ReadingSection({ quest }) {
   return (
     <div>
       <h1>Reading Section</h1>
+      <p>{quest.reading.text}</p>
     </div>
   );
 }
