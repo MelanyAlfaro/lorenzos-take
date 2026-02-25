4a439718a95aca0ce671699f89ca81148a2d614e
Melany Alfaro <melanygabriela.alfaro@ucr.ac.cr>
2026-02-25 09:58:06 -0600

    feat: implement QuestSection structure

--- src/pages/Student/QuestSection.jsx
+++ src/pages/Student/QuestSection.jsx
@@ -1,7 +1,39 @@
+import "./QuestSection.css";
+import { quests } from "../../data/quests";
+import completedIcon from "../../assets/completedIcon.png";
+
 export function QuestSection() {
   return (
-    <div>
-      <h1>Quest Section</h1>
+    <div className="quest-section">
+      <h2 className="available-quest-text">Available Quest</h2>
+      <div className="quest-grid">
+        {quests.map((quest) => {
+          return (
+            <div className="quest-card" key={quest.id}>
+              <h2 className="quest-title">{quest.title}</h2>
+
+              <div className="sub-header">
+                {quest.completed ? (
+                  <>
+                    <img src={completedIcon} className="completed-icon" />
+                    <p className="completed-label">Completed</p>
+                  </>
+                ) : (
+                  <p className="quest-xp">50 XP</p>
+                )}
+              </div>
+
+              <button
+                className={
+                  quest.completed ? "completed-button" : "start-button"
+                }
+              >
+                {quest.completed ? "50 XP" : "Start Quest"}
+              </button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
